4e472fbda70bba8056142d35c4fe2c5be9ba38c4
Andreina <rei.acee@gmail.com>
2024-06-10 10:27:58 -0400

    Initiated

---
+++ script.js
@@ -0,0 +1,28 @@
+domcument.addEventListener('DOMContentLoaded', () => {
+    const billAmountInput = document.getElementById('billAmount')
+    const numPeopleInput = document.getElementById('numPeople')
+    const tipPercentageInput = document.getElementById('tipPercentage')
+    const calculateBtn = document.getElementById('calculateBtn')
+    const resultDiv = document.getElementById('result')
+
+    calculateBtn.addEventListener('click', calculateBill)
+
+    function calculateBill() {
+        const billAmount = parseFloat(billAmountInput.value)
+        const numPeople = parseInt(numPeopleInput.value)
+        const tipPercentage = parseFloat(tipPercentageInput.value)
+
+        if (isNaN(billAmount) || isNaN(numPeople) || isNaN(tipPercentage) || billAmount <= 0 || numPeople <= 0 || tipPercentage < 0) {
+            resultDiv.textContent = 'please enter valid numbers.'
+            return
+        }
+
+        const tipAmount = (billAmount * (tipPercentage / 100))
+        const totalAmount = billAmount + tipAmount
+        const amountPerPerson = totalAmount / numPeople
+
+        resultDiv.textContent = `Total Amount: $${totalAmount.toFixed(2)}
+        Tip Amount: $${tipAmount.toFixed(2)}
+        Each Person Owes: $${amountPerPerson.toFixed(2)}`;
+    }
+});
